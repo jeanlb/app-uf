d7d77e3c21bfa26a77509959b836e0e37b78fa89
jeanlb <brasil.jeanlouis@gmail.com>
2019-11-17 02:13:50 -0300

    Removendo pasta database

--- database/create_h2_uf_table.sql
+++
@@ -1,9 +0,0 @@
--- criacao de tabela para o caso de usar o banco de dados H2
-CREATE TABLE unidade_federativa ( 
-   id BIGINT NOT NULL AUTO_INCREMENT, 
-   nome_estado VARCHAR(50) NOT NULL, 
-   uf VARCHAR(2) NOT NULL, 
-   data_cadastro DATETIME DEFAULT NULL 
-);
-
-INSERT INTO unidade_federativa(nome_estado, uf, data_cadastro) values('Santa Catarina', 'SC', PARSEDATETIME('2019-11-10 00:00:00', 'yyyy-MM-dd hh:mm:ss'));
